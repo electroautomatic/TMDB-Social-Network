d4af095d5ec3459fb26b6f1852e563da3561816d
Anton <electroautomatic@gmail.com>
2025-04-04 21:15:06 +0500

    feat: implement password reset functionality

--- tmdb_net/settings.py
+++ tmdb_net/settings.py
@@ -170,3 +170,13 @@ LOGIN_URL = 'login'  # URL to redirect for login when using @login_required
 # TMDB API Settings
 TMDB_API_KEY = os.environ.get('TMDB_API_KEY', '')
 TMDB_API_URL = 'https://api.themoviedb.org/3'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', 'noreply@kino-bro.ru')
